8891c8ac9debc6069a6ad8b87932900a3173674b
Ishikura <j1504@matsue-ct.jp>
2018-11-07 11:13:14 +0900

    Change If sytax

--- j1504_fizzbuzz.rb
+++ j1504_fizzbuzz.rb
@@ -1,13 +1,13 @@
 #! ruby -ku
 
 for num in 1..100 do
-    if (i % 3 == 0 && i % 5 == 0){
+    if num % 3 == 0 || num % 5 == 0 then
       printf("FizzBuzz\n");
-    } else if (i % 3 == 0) {
+    elsif num % 3 == 0 then
       printf("Fizz\n");
-    } else if (i % 5 == 0) {
+    elsif i % 5 == 0 then
       printf("Buzz\n");
-    } else {
+    else 
       printf("%d\n", i);
-    }
+    end
 end
